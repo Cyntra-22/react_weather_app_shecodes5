aead5958c9152e670c561ba7161c5836d9c9d92c
Cyntra-22 <140875968+Cyntra-22@users.noreply.github.com>
2023-12-17 14:11:18 +0700

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import './App.css';
+import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <p>Weather App</p>
-      <footer>
-        This project is created by Myo Pa Pa Kyaw and is
-        <a href="" target="_blank">open-source</a>on GitHub.
-      </footer>
+    <div className="App ">
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is created by Myo Pa Pa Kyaw and is
+          <a
+            href="https://github.com/Cyntra-22/react_weather_app_shecodes5"
+            
+          >
+            {" "}
+            open-source{" "}
+          </a>
+          on GitHub.
+        </footer>
+      </div>
     </div>
   );
 }
-
-
