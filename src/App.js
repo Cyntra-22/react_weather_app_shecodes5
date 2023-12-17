80783ec375bacfdb4238275d383a8040c79d58e1
Cyntra-22 <140875968+Cyntra-22@users.noreply.github.com>
2023-12-17 15:36:28 +0700

    Added the link to my website

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import "./App.css";
-import Weather from "./Weather"
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App ">
       <div className="container">
-        <Weather/>
+        <Weather />
         <footer>
-          This project is created by Myo Pa Pa Kyaw and is
-          <a
-            href="https://github.com/Cyntra-22/react_weather_app_shecodes5" 
-            
-          >
+          This project was coded by{" "}
+          <a href="https://grand-moxie-36c236.netlify.app/"> Myo Pa Pa Kyaw</a>{" "}
+          and is
+          <a href="https://github.com/Cyntra-22/react_weather_app_shecodes5">
             {" "}
             open-source{" "}
           </a>
