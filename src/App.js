3870873905905d2754267423d4763004c9463977
Cyntra-22 <140875968+Cyntra-22@users.noreply.github.com>
2023-12-17 13:48:12 +0700

    Added the footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ export default function App() {
   return (
     <div className="App">
       <p>Weather App</p>
+      <footer>
+        This project is created by Myo Pa Pa Kyaw and is
+        <a href="" target="_blank">open-source</a>on GitHub.
+      </footer>
     </div>
   );
 }
